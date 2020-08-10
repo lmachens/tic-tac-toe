aaa27b1315734f771a0037b30da36daed97b2ce1
lmachens <leon.machens@googlemail.com>
2020-08-10 18:43:28 +0200

    Refactor board table

--- src/components/Board.js
+++ src/components/Board.js
@@ -8,17 +8,13 @@ export default function Board() {
   return (
     <div>
       <div className="status">{status}</div>
-      <div className="board-row">
+      <div className="board">
         <Square />
         <Square />
         <Square />
-      </div>
-      <div className="board-row">
         <Square />
         <Square />
         <Square />
-      </div>
-      <div className="board-row">
         <Square />
         <Square />
         <Square />
