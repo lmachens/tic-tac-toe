18b7163b028491d1073dc74b1496c138d7ce96d9
lmachens <leon.machens@googlemail.com>
2020-10-26 08:43:34 +0100

    Add keyboard navigation

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./app.css";
 import Game from "./components/Game";
 
 function App() {
+  useEffect(() => {
+    window.addEventListener("mousedown", function () {
+      document.body.classList.add("mouse-navigation");
+      document.body.classList.remove("kbd-navigation");
+    });
+    window.addEventListener("keydown", function (e) {
+      if (e.key === "Tab") {
+        document.body.classList.add("kbd-navigation");
+        document.body.classList.remove("mouse-navigation");
+      }
+    });
+    window.addEventListener("click", function (e) {
+      if (e.target.tagName === "A" && e.target.getAttribute("href") === "#") {
+        e.preventDefault();
+      }
+    });
+  }, []);
+
   return (
     <div className="app">
       <Game />
